12a9bc8be05a6982fdf8000e2652d411be5412d0
Grégoire Payen de La Garanderie <gdlg@hochet.info>
2020-10-28 19:53:25 +0000

    Update the links to the pretrained weights and dataset.

--- download_models.sh
+++ download_models.sh
@@ -1,5 +1,5 @@
 mkdir -p models
-wget https://hades.ext.dur.ac.uk/~greg/weights/panoramic_depth_estimation_checkpoints.tar.gz
+wget https://hochet.info/~gregoire/eccv2018/weights/panoramic_depth_estimation_checkpoints.tar.gz
 tar xzf panoramic_depth_estimation_checkpoints.tar.gz -C models
 rm panoramic_depth_estimation_checkpoints.tar.gz
 
